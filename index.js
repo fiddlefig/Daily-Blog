c51bb70339d01e7fea33760f03039a6735066f60
Sadaja <83880352+fiddlefig@users.noreply.github.com>
2023-11-24 15:47:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function colorChange () {
+    document.getElementById('color2').style.backgroundColor = "black";
+}
+function colorChange2 () {
+    document.getElementById('color').style.backgroundColor = "orange";
+}
+
